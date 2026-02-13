0912c412ff42dfc0b3eed08d8de5bf96a5a90954
tahnflakf <165201778+tahnflakf@users.noreply.github.com>
2026-02-13 23:51:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function clearInput() {
 // Hàm kiểm tra mật khẩu
 function checkPassword() {
     // >>> SỬA MẬT KHẨU CỦA BẠN Ở ĐÂY <<<
-    const CORRECT_PASS = "2011"; 
+    const CORRECT_PASS = "140125"; 
     
     const inputVal = document.getElementById("password-input").value;
     const errorMsg = document.getElementById("error-message");
